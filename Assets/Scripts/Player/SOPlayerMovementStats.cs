bb9caffa1da49599a401b75f4666cb1b2dd4c820
Cesar Betancourt <cessarr862@gmail.com>
2026-09-09 16:19:14 -0600

    update: player movement stats on so

--- Assets/Scripts/Player/SOPlayerMovementStats.cs
+++ Assets/Scripts/Player/SOPlayerMovementStats.cs
@@ -18,4 +18,38 @@ public class SOPlayerMovementStats : ScriptableObject
     public float GroundDetectionRayLength = 0.02f;
     public float HeadDetectionRayLength = 0.02f;
     [Range(0f, 1f)] public float HeadWidth = 0.75f;
+
+    [Header("Jump")]
+    public float JumpHeight = 6.5f;
+    [Range(1f, 1.1f)] public float JumpHeightCompensationFactor = 1.054f;
+    public float TimeTillJumpApex = 0.35f;
+    [Range(0.01f, 5f)] public float GravityOnReleaseMultiplier = 2f;
+    public float MaxFallSpeed = 26f;
+    [Range(1, 5)] public int NumberOfJumpsAllowed = 2;
+
+    [Header("Jump Cut")]
+    [Range(0.02f, 0.3f)] public float TimeForUpwardsCancel = 0.027f;
+
+    [Header("Jump Apex")]
+    [Range(0.5f, 1f)] public float ApexThreshold = 0.97f;
+    [Range(0.01f, 1f)] public float ApexHangTime = 0.075f;
+
+    [Header("Jump Buffer")]
+    [Range(0f, 1f)] public float JumpBufferTime = 0.125f;
+
+    [Header("Jump Coyote Time")]
+    [Range(0f, 1f)] public float JumpCoyoteTime = 0.1f;
+
+    [Header("Debug")]
+    public bool DebugShowIsGroundedBox;
+    public bool DebugShowHeadBumpBox;
+
+    [Header("JumpVisualization Tool")]
+    public bool ShowWalkJumpArc = false;
+    public bool ShowRunJumpArc = false;
+    public bool StopOnCollision = true;
+    public bool DrawRight = true;
+    [Range(5, 100)] public int ArcResolution = 20;
+    [Range(0, 500)] public int VisualizationSteps = 90;
+
 }
